0236bdbafff53edcea7728f7d4516d9077d50bb3
abhishek <aksharma.27mjm@gmail.com>
2024-07-18 14:19:59 +0530

    DAY - 5, FEW MORE TO DO FOR TODAY :

---
+++ src/com/company/AddBinary.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class AddBinary {
+
+        static String addBinary(String a, String b) {
+            StringBuilder res = new StringBuilder();
+            int i = a.length() - 1;
+            int j = b.length() - 1;
+            int carry = 0;
+            while(i >= 0 || j >= 0){
+                int sum = carry;
+                if(i >= 0){
+                    sum += a.charAt(i) - '0';
+                    i--;
+                }
+                if(j >= 0){
+                    sum += b.charAt(j) - '0';
+                    j--;
+                }
+                carry = sum > 1 ? 1 : 0;
+                res.append(sum % 2);
+            }
+            if(carry != 0){
+                res.append(carry);
+            }
+            return res.reverse().toString();
+        }
+
+    public static void main(String[] args) {
+        String a = "1010";
+        String b = "1";
+        System.out.println(addBinary(a, b));
+    }
+
+}
